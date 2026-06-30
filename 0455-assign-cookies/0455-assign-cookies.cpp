e84f4eeac60964a7e5e2e5a638f1abf88aa0de79
BHAVANA SATAM <bhavanasatam07@gmail.com>
2026-06-30 21:15:13 +0530

    Time: 14 ms (19.1%), Space: 44.8 MB (47.95%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& Student, vector<int>& Cookie) {
+
+        sort(Student.begin(), Student.end());
+        sort(Cookie.begin(), Cookie.end());
+
+        int i = 0;
+        int j = 0;
+
+        while (i < Student.size() && j < Cookie.size()) {
+
+            if (Cookie[j] >= Student[i]) {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i;
+    }
+};
