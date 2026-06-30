61038beae86233dcb9c8c01d5a96fa389888b697
BHAVANA SATAM <bhavanasatam07@gmail.com>
2026-06-30 21:14:18 +0530

    Time: 0 ms (100%), Space: 20.8 MB (29.04%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int currEnd = 0;
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            maxReach = max(maxReach, i + nums[i]);
+
+            if (i == currEnd) {
+                jumps++;
+                currEnd = maxReach;
+            }
+        }
+
+        return jumps;
+    }
+};
