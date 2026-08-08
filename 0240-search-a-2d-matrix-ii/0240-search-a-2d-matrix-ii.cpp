15c35a6e79023949c6511c0471be05810b88766f
BHAVANA SATAM <bhavanasatam07@gmail.com>
2026-08-09 03:26:28 +0530

    Time: 57 ms (37.57%), Space: 18.6 MB (93.47%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        int row = 0;
+        int col = columns -1;
+        while (row < rows && col >=0){
+            if (matrix[row][col] == target){
+                return true;
+            }
+            else if (matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
+            }
+        }
+       return false;
+    }
+};
